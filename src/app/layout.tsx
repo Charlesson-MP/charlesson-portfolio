d50efb5bb09788dd754eabb172b2a413ce647024
Charlesson-MP <charlessonmendes@gmail.com>
2026-04-10 23:06:02 -0300

    refactor: update themeProvider import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 import { Header } from "@/components/layout/Header";
 
 const geistSans = Geist({
